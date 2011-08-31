b9f473eda30b903cd9624980184476958fdf2bd9
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2011-08-31 14:20:19 -0300

    Legalese to prevent mindless sed'ing

--- jurt-suid.c
+++ jurt-suid.c
@@ -1,3 +1,24 @@
+/** 
+ Copyright (c) 2011 Bogdano Arendartchuk <bogdano@mandriva.com.br>
+
+ Written by Bogdano Arendartchuk <bogdano@mandriva.com.br>
+
+ This file is part of Jurt Build Bot.
+
+ Jurt Build Bot is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published
+ by the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
+
+ Jurt Build Bot is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Jurt Build Bot; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 /** Small wrapper around jurt-root-command, as an alternative to sudo */
 #include <unistd.h>
 #include <sys/types.h>
